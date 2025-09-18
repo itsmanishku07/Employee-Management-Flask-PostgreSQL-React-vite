409a6876b73653c92d2455a1a785b4673a3d3a15
itsmanishku07 <manish.kumar@7dxperts.com>
2025-09-18 15:13:56 +0530

    Title Name Updated

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -32,7 +32,7 @@ const Login = () => {
         e.preventDefault();
 
         try {
-            const response = await fetch("http://127.0.0.1:5000/login", {
+            const response = await fetch("https://dashboard-alpha-nine-93.vercel.app/login", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ email, password }),
